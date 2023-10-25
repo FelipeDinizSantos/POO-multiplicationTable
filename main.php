563f3c2f4162f64d174a7531d7f0384cf10db858
Felipe Diniz <FelipeDinizdosSantos0@gmail.com>
2023-10-25 11:02:37 -0300

    Update main.php

--- main.php
+++ main.php
@@ -9,7 +9,7 @@
         {
             SetStyles::set();
             GenerateMultiplicationTable::generate(); // O método espera o valor range de quantas tabuadas irá gerar
-                                                     // Caso não forneça os valores padrões são 1 e 10
+                                                     // Caso não forneça, os valores padrões são 1 e 10
         }
     }
 
